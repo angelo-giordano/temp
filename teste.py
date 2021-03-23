89c3d65d0784488c9762ccb2d458a254681aed44
Angelo Giordano <angelo.giordano.silveira@hotmail.com>
2021-03-23 19:07:57 -0300

    changes

--- teste.py
+++ teste.py
@@ -1,28 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-from math import sqrt
 
 
 def func(x, a, b):
     return a*(x**b)
 
 
-def media(valores):
-    return (sum(valores)/len(valores))
-
-
-def desvio_padrao(valores):
-    m = media(valores)
-    nova_lista = []
-    for v in valores:
-        nova_lista.append((v-m)**2)
-
-    valor = sqrt(sum(nova_lista))
-    return valor/len(nova_lista)
-
-
-massa = 5
+massa_zero = 5
+errmassa_zero = 0.05 # erro instrumental da balança prof
 
 values1 = np.array([4.6, 4, 4.4, 4, 4.7, 3.9, 4.3, 4.3])
 values2 = np.array([2.6, 2.5, 2.7, 3.1, 2.5, 2.8, 2.9, 3])
@@ -35,34 +21,32 @@ values8 = np.array([0.4, 0.4, 0.3, 0.3, 0.3, 0.3, 0.3, 0.4])
 values9 = np.array([0.2, 0.2, 0.1, 0.1, 0.2, 0.1, 0.1, 0.2])
 values10 = np.array([0, 0, 0, 0.1, 0.2, 0.1, 0, 0.1])
 
-x = np.array([media(values1), media(values2), media(values3), media(values4),
-              media(values5), media(values6), media(values7), media(values8),
-              media(values9), media(values10)])
+dadosdiam = [values1,values2,values3,values4,values5,values6,values7,values8,values9,values10]
 
-y = np.array([massa, massa/2, massa/(2**2), massa/(2**3), massa/(2**4),
-              massa/(2**5), massa/(2**6), massa/(2**7), massa/(2**8),
-              massa/(2**9)])
+diam_medio = [np.average(diam) for diam in dadosdiam]
+massa = [massa_zero/(2**i) for i in range(10)]
+errdiam = [np.std(diam/np.sqrt(len(diam))) for diam in dadosdiam]
+errmassa = [errmassa_zero/(2**(i)) for i in range(10)]
+
+initial_guess = [2.5, 2.5]
+ans, err = curve_fit(func, diam_medio, massa, initial_guess)
 
 plt.title('Fractais bolinha de papel')
-
-yerr = 0.5
-xerr = 0.05
-
 plt.ylabel('Massa (g)')
 plt.xlabel('Diâmetro médio (cm)')
 
-plt.errorbar(x, y, xerr=xerr,  yerr=yerr, fmt=' ',
-             color='black', label='Incertezas')
 
-params, extras = curve_fit(func, x, y)
+plt.plot(diam_medio, massa, marker='o', ls='', color='black', label='Dados experimentais')
 
-a, b = params
+plt.errorbar(diam_medio, massa, xerr=errdiam,  yerr=errmassa, fmt=' ',
+             color='black')
 
-plt.plot(x, func(x, a, b), 'r',
-         label=f'Parâmetros de treinamento: a={round(a, 3)}, b={round(b, 3)}' +
-         '\nFunction: a*(x**b)')
+plt.plot(np.arange(0.0, 5.0, 0.01), func(np.arange(0.0, 5.0, 0.01), *ans), 'r',
+         label= f"Parâmetros de ajuste função a*(x**b): \n a = {ans[0]:.4f} +/- {err[0,0]:.4f}"
+         f"\n b = {ans[1]:.4f} +/- {err[1,1]:.4f}")
 
-plt.plot(x, y, marker='.', ls='', color='black', label='Dados experimentais')
+quadrado_da_soma = sum(((massa-func(diam_medio, *ans)/errmassa))**2)
+plt.figtext(0.135, 0.73, f'chi-square: {quadrado_da_soma:.2f}')
 
 plt.legend()
 plt.show()
